4645128066abdc29bdeceaac4127d9026e254cec
lokuciejewski <grododzierzca@gmail.com>
2023-04-24 12:46:37 +0200

    removed debug prints

--- src/modules/sources/text_source.rs
+++ src/modules/sources/text_source.rs
@@ -19,10 +19,8 @@ impl TextSource {
 
 impl Sourceable for TextSource {
     fn get_new_sentence(&self) -> Result<String, String> {
-        println!("All sentences: {:?}", self.sentences);
         match self.sentences.choose(&mut thread_rng()) {
             Some(s) => {
-                println!("Returned: {}", s);
                 Ok(s.to_owned())
             }
             None => Err("Could not get a sentence!".to_string()),
